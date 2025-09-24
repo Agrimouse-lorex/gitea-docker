297a849abac3fbc385b9612df4866304624ac864
Tromsa Oleksandr <agrimouse@gmail.com>
2025-09-24 09:44:32 +0300

    no e2e tests

--- playwright2.config.ts
+++ playwright2.config.ts
@@ -15,7 +15,7 @@ dotenv.config();
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_WEB_URL,
+    baseURL: "https://google.com",
     
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
