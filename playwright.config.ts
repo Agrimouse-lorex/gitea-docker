79c33590a3d15267b54864c2550722bb7e0f1aed
Agrimouse-lorex <agrimouse@gmail.com>
2025-12-09 14:49:28 +0200

    fixign playwright-report with videos and screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_WEB_URL,
-    
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'id',
